89ba0a627a7cf93ef998c23dc07a6995c12fa1eb
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2014-04-26 11:51:22 +0200

    Init.

---
+++ main.cpp
@@ -0,0 +1,106 @@
+/* 
+ * File:   main.cpp
+ * Author: morgan
+ *
+ * Created on July 30, 2013, 8:34 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <ogli/time.h>
+
+#include <mo/Renderer.h>
+#include <mo/Model.h>
+
+
+float resolution_x = 1280.0f;
+float resolution_y = 800.0f;
+
+auto projection_ = glm::perspective(45.0f, resolution_x / resolution_y, 1.0f, 1000.f);
+auto view = glm::lookAt(glm::vec3(0.0f, -10.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+void error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+        
+    };
+    if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
+       
+    };
+    if (key == GLFW_KEY_D) {
+        
+    };
+    if (key == GLFW_KEY_D && action == GLFW_RELEASE) {
+        
+    };
+    if (key == GLFW_KEY_W){
+       
+    };
+    if (key == GLFW_KEY_W && action == GLFW_RELEASE){
+        
+    }
+    if (key == GLFW_KEY_S){
+        
+    };
+    if (key == GLFW_KEY_S && action == GLFW_RELEASE){
+        
+    }
+    if (key == GLFW_KEY_SPACE ){
+    
+    }
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //TODO: define vertex format in main 
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+    glfwSetErrorCallback(error_callback);
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    GLFWwindow* window = glfwCreateWindow(resolution_x, resolution_y, "Ludum", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSwapInterval(1);
+    
+    //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+    
+    
+    mo::Renderer renderer;    
+    
+    double frame_time = 0.0;
+    while (!glfwWindowShouldClose(window)) {
+        double old_time = ogli::now_ms();      
+       
+        renderer.clear(glm::vec3(1.0f, 0.0f, 0.0f));
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();        
+        frame_time = ogli::now_ms() - old_time;
+    }
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-x32-win32-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-x32-win32-Windows
+CND_ARTIFACT_NAME_Debug=ludum_dare_29
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-x32-win32-Windows/ludum_dare_29
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-x32-win32-Windows/package
+CND_PACKAGE_NAME_Debug=ludumdare29.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-x32-win32-Windows/package/ludumdare29.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-x32-win32-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-x32-win32-Windows
+CND_ARTIFACT_NAME_Release=ludum_dare_29
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-x32-win32-Windows/ludum_dare_29
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-x32-win32-Windows/package
+CND_PACKAGE_NAME_Release=ludumdare29.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-x32-win32-Windows/package/ludumdare29.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
