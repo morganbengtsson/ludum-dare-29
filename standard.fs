e7015084d7c3242746bea5698e35b7442cc0da9b
morgan <bengtsson.morgan@gmail.com>
2014-04-26 18:34:29 +0200

    Light

--- standard.fs
+++ standard.fs
@@ -1,9 +1,6 @@
-#ifdef GL_ES
-precision mediump float;
-precision mediump int;
-#endif
 uniform vec4 color;
 uniform sampler2D texture;
+uniform vec3 camera_position;
 varying vec3 v_position;
 varying vec3 v_normal; 
 varying vec3 v_position2;
@@ -12,18 +9,13 @@ void main() {
 vec3 ambient = vec3(0.3, 0.3, 0.3);
 float d = (v_position.z + 100.0)/5000.0;
 
-vec3 light = normalize(vec3(0.0, 500.0, 100.0) - v_position2);
+vec3 light = normalize(camera_position - v_position2);
 float intensity = max(dot(v_normal,light), 0.0) * 0.2;        
 
 vec3 indirect = texture2D(texture, v_uv).rgb;
 
+vec2 normalized_frag = vec2((gl_FragCoord.x / 1280.0)*2.0 - 1.0, (gl_FragCoord.y / 800.0)*2.0 - 1.0);
+float test = length(normalized_frag);
 gl_FragColor = vec4(indirect + intensity, 1.0);
-
-//gl_FragColor = vec4(v_position, 1.0);
-
-//gl_FragColor = vec4(color.xyz, 1.0);
-
-//gl_FragColor = vec4(v_uv.xy, 1.0, 1.0);
-//gl_FragColor = vec4(texture2D(texture, v_uv).rgb, 1.0);
-//gl_FragColor = vec4(v_normal, 1.0); 
+//gl_FragColor = vec4(test.xxx, 1.0);
 }
