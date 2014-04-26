21eda0dd0a2255cb5895b4ac518dc2777082362c
morgan <bengtsson.morgan@gmail.com>
2014-04-26 18:11:36 +0200

    Strange netbeans problem fixed.

---
+++ submarine.fs
@@ -0,0 +1,29 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+uniform vec4 color;
+uniform sampler2D texture;
+varying vec3 v_position;
+varying vec3 v_normal; 
+varying vec3 v_position2;
+varying vec2 v_uv;
+void main() {
+vec3 ambient = vec3(0.3, 0.3, 0.3);
+float d = (v_position.z + 100.0)/5000.0;
+
+vec3 light = normalize(vec3(0.0, 500.0, 100.0) - v_position2);
+float intensity = max(dot(v_normal,light), 0.0) * 0.2;        
+
+vec3 indirect = texture2D(texture, v_uv).rgb;
+
+gl_FragColor = vec4(indirect + intensity, 1.0);
+
+//gl_FragColor = vec4(v_position, 1.0);
+
+//gl_FragColor = vec4(color.xyz, 1.0);
+
+//gl_FragColor = vec4(v_uv.xy, 1.0, 1.0);
+//gl_FragColor = vec4(texture2D(texture, v_uv).rgb, 1.0);
+//gl_FragColor = vec4(v_normal, 1.0); 
+}
---
+++ submarine.vs
@@ -0,0 +1,21 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+uniform mat4 model_view_projection; 
+uniform mat4 model_view; 
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+varying vec3 v_position;
+varying vec3 v_normal;
+varying vec3 v_position2;
+varying vec2 v_uv;
+void main()
+{
+    v_uv = uv;
+    v_position2 = normalize((model_view_projection * vec4(position, 1.0)).xyz);
+    v_position = (model_view * vec4(position, 0.0)).xyz;
+    v_normal = normalize((model_view * vec4(normal, 0.0)).xyz);
+    gl_Position = model_view_projection * vec4(position, 1.0);
+}
