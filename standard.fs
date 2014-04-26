95e2c21a8bf30b3950f6d659f7c80f019e96654b
morgan <bengtsson.morgan@gmail.com>
2014-04-26 21:36:09 +0200

    No light for now

--- standard.fs
+++ standard.fs
@@ -1,17 +1,29 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
 uniform vec4 color;
 uniform sampler2D texture;
-uniform vec3 camera_position;
 varying vec3 v_position;
 varying vec3 v_normal; 
 varying vec3 v_position2;
 varying vec2 v_uv;
 void main() {
 vec3 ambient = vec3(0.3, 0.3, 0.3);
+float d = (v_position.z + 100.0)/5000.0;
 
-vec3 light = normalize(vec3(0.0,0.0,0.0) - v_position2);
+vec3 light = normalize(vec3(0.0, 0.0, 0.0) - v_position2);
+float intensity = max(dot(v_normal,light), 0.0) * 0.5;        
 
-vec3 material = texture2D(texture, v_uv).rgb;
-vec3 diffuse = max(dot(v_normal,light), 0.0);
+vec3 indirect = texture2D(texture, v_uv).rgb;
 
-gl_FragColor = vec4(diffuse, 1.0);
+gl_FragColor = vec4(indirect, 1.0);
+
+//gl_FragColor = vec4(v_position, 1.0);
+
+//gl_FragColor = vec4(color.xyz, 1.0);
+
+//gl_FragColor = vec4(v_uv.xy, 1.0, 1.0);
+//gl_FragColor = vec4(texture2D(texture, v_uv).rgb, 1.0);
+//gl_FragColor = vec4(v_normal, 1.0); 
 }
